f48a169b7305ae9773b5db4ebde360fc3454cb8c
tech_nickk <101123593+tech-nickk@users.noreply.github.com>
2024-05-05 17:50:12 +0300

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,18 +1,6 @@
-// Get the consent status from storage
-chrome.storage.sync.get('consent', function(data) {
-    const consentStatus = data.consent || 'unknown';
-    const consentStatusElement = document.getElementById('consentStatus');
-    consentStatusElement.textContent = `Consent status: ${consentStatus}`;
-  });
-  
-  // Add event listener to the "Manage Consent" button
-  document.getElementById('toggleConsent').addEventListener('click', function() {
-    chrome.storage.sync.get('consent', function(data) {
-      const currentConsent = data.consent || 'unknown';
-      const newConsent = currentConsent === 'true' ? 'false' : 'true';
-      chrome.storage.sync.set({ 'consent': newConsent }, function() {
-        console.log('Consent status updated to:', newConsent);
-        location.reload();
-      });
-    });
+// popup.js
+
+document.getElementById('learnMore').addEventListener('click', () => {
+    // Open a new tab or window with the "Learn More" page
+    window.open('https://www.odpc.go.ke/', '_blank');
   });
